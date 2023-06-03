0f51fdfcf262ceb01d7675c2c09fe25894ba981e
Joao <jo4o.fds@gmail.com>
2023-06-02 22:15:06 -0300

    adiciona rotas de api para produtos

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// ROOT
+Route::get('/', function () {
+    return response()->json(
+        [
+            'message' => 'Adoorei API'
+        ]
+    );
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('api')->group(function () {
+    // PRODUTOS
+    Route::apiResource('product', ProductController::class)
+    ->except(['create', 'edit']);
 });
