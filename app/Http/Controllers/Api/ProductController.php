942e0e0b9d33168059de15723ab40de4088b5ef6
Joao <jo4o.fds@gmail.com>
2023-06-02 22:23:00 -0300

    listando todos os produtos

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -17,7 +17,25 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $products = Product::all();
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'data' => [],
+                    'message' => $e->getMessage()
+                ],
+                JsonResponse::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+
+        return response()->json(
+            [
+                'data' => $products,
+                'message' => 'success'
+            ],
+            JsonResponse::HTTP_OK
+        );
     }
 
     /**
